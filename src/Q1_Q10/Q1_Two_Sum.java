db3a76f66b621da79bb09684b98075b076b516a8
dglalperen <alperen.dagli43@gmail.com>
2022-10-31 21:51:21 +0100

    Q1 finished

--- src/Q1_Q10/Q1_Two_Sum.java
+++ src/Q1_Q10/Q1_Two_Sum.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Q1_Two_Sum {
     public static void main(String[] args) {
-        int [] nums = {2,7,11,15};
-        int target = 18;
+        int [] nums = {2,5,5,11};
+        int target = 10;
 
         int[] result = twoSum(nums, target);
 
@@ -14,7 +14,7 @@ public class Q1_Two_Sum {
 
     public static int[] twoSum(int[] nums, int target){
         for(int i = 0; i < nums.length; i++){
-            for(int j = 1; j<nums.length; j++){
+            for(int j = i + 1; j<nums.length; j++){
                 if(nums[i] + nums[j] == target){
                     return new int[]{i,j};
                 }
